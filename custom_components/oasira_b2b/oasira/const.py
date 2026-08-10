3609c55a8b03fbd589b4bbf41295e4a1f4844b6c
EH Admin <jermie@effortlesshome.co>
2026-08-10 16:52:00 -0400

    latest

---
+++ custom_components/oasira_b2b/oasira/const.py
@@ -0,0 +1,10 @@
+"""Constants for Oasira API."""
+
+# API Base URLs
+SECURITY_API = "https://securityapi.effortlesshome.co/"
+CUSTOMER_API = "https://cust.effortlesshome.co/"
+
+# Firebase Authentication
+FIREBASE_AUTH_URL = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
+FIREBASE_TOKEN_URL = "https://securetoken.googleapis.com/v1/token"
+FIREBASE_USER_INFO_URL = "https://identitytoolkit.googleapis.com/v1/accounts:lookup"
